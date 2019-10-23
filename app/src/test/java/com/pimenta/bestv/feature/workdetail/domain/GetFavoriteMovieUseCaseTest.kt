a3fef2c688dd51504da89543c6d89575c58c6d98
Marcus Pimenta <mvinicius.pimenta@gmail.com>
2019-10-23 17:57:54 +0100

    Add unit tests for get favorite work use cases

---
+++ app/src/test/java/com/pimenta/bestv/feature/workdetail/domain/GetFavoriteMovieUseCaseTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Marcus Pimenta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.pimenta.bestv.feature.workdetail.domain
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.pimenta.bestv.data.MediaDataSource
+import com.pimenta.bestv.data.local.entity.MovieDbModel
+import io.reactivex.Maybe
+import org.junit.Test
+
+/**
+ * Created by marcus on 23-10-2018.
+ */
+private const val MOVIE_ID = 1
+private val MOVIE_DB_MODEL = MovieDbModel(
+        id = 1
+)
+
+class GetFavoriteMovieUseCaseTest {
+
+    private val mediaDataSource: MediaDataSource = mock()
+
+    private val useCase = GetFavoriteMovieUseCase(
+            mediaDataSource
+    )
+
+    @Test
+    fun `should return when loading a favorite movie`() {
+        whenever(mediaDataSource.getFavoriteMovie(MOVIE_ID))
+                .thenReturn(Maybe.just(MOVIE_DB_MODEL))
+
+        useCase(MOVIE_ID)
+                .test()
+                .assertComplete()
+                .assertResult(MOVIE_DB_MODEL)
+    }
+
+    @Test
+    fun `should return an error when some exception happens when checking if a movie is favorite`() {
+        whenever(mediaDataSource.getFavoriteMovie(MOVIE_ID))
+                .thenReturn(Maybe.error(Throwable()))
+
+        useCase(MOVIE_ID)
+                .test()
+                .assertError(Throwable::class.java)
+    }
+}
---
+++ app/src/test/java/com/pimenta/bestv/feature/workdetail/domain/GetFavoriteTvShowUseCaseTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Marcus Pimenta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.pimenta.bestv.feature.workdetail.domain
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.pimenta.bestv.data.MediaDataSource
+import com.pimenta.bestv.data.local.entity.TvShowDbModel
+import io.reactivex.Maybe
+import org.junit.Test
+
+/**
+ * Created by marcus on 23-10-2018.
+ */
+private const val TV_SHOW_ID = 1
+private val TV_SHOW_DB_MODEL = TvShowDbModel(
+        id = 1
+)
+
+class GetFavoriteTvShowUseCaseTest {
+
+    private val mediaDataSource: MediaDataSource = mock()
+
+    private val useCase = GetFavoriteTvShowUseCase(
+            mediaDataSource
+    )
+
+    @Test
+    fun `should return when loading a favorite tv show`() {
+        whenever(mediaDataSource.getFavoriteTvShow(TV_SHOW_ID))
+                .thenReturn(Maybe.just(TV_SHOW_DB_MODEL))
+
+        useCase(TV_SHOW_ID)
+                .test()
+                .assertComplete()
+                .assertResult(TV_SHOW_DB_MODEL)
+    }
+
+    @Test
+    fun `should return an error when some exception happens when checking if a tv show is favorite`() {
+        whenever(mediaDataSource.getFavoriteTvShow(TV_SHOW_ID))
+                .thenReturn(Maybe.error(Throwable()))
+
+        useCase(TV_SHOW_ID)
+                .test()
+                .assertError(Throwable::class.java)
+    }
+}
