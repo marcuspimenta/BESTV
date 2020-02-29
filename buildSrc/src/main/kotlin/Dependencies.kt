d0c8677629748cebcb93013e1ffe773a021f21f0
Marcus Pimenta <mvinicius.pimenta@gmail.com>
2020-02-29 10:45:55 +0000

    Update Androidx Room

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -35,7 +35,7 @@ object Versions {
     val androidx_lifecycle = "2.2.0-rc03"
     val androidx_recommendation = "1.0.0"
     val androidx_tvprovider = "1.0.0"
-    val androidx_room = "2.2.3"
+    val androidx_room = "2.2.4"
     val androidx_work = "2.3.0-rc01"
 
     val dagger = "2.25.4"
