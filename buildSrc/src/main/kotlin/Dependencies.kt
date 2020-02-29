c90a64e8d8a0c5fb327316f42ea790318aff27e8
Marcus Pimenta <mvinicius.pimenta@gmail.com>
2020-02-29 10:48:55 +0000

    Update Mockito

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -52,8 +52,8 @@ object Versions {
     val lottie = "3.3.1"
     val rxandroid = "2.1.1"
     val junit = "4.13"
-    val mockito_inline = "3.2.4"
-    val mockito_core = "3.2.4"
+    val mockito_inline = "3.3.1"
+    val mockito_core = "3.3.1"
     val mockito_kotlin = "2.1.0"
 }
 
